10719b1e580c4f00ad883c46e861a75da531b03a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 23:54:01 +0800

    Create full_version.cpp

---
+++ full_version.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string Count(string s) {
+	int n = s.size();
+	string ss;
+	int cnt = 1;
+	for (int i = 0; i < n - 1; i++) {
+		if (s[i] == s[i + 1]) {
+			cnt++;
+		} else {
+			ss += to_string(cnt);
+			ss += s[i];
+			cnt = 1;
+		}
+	}
+	ss += to_string(cnt);
+	ss += s[n - 1];
+	return ss;
+}
+
+string CountAndSay(int n) {
+	vector<string> v(n + 1);
+	v[0] = "1";
+	for (int i = 1; i <= n; i++) {
+		v[i] = Count(v[i - 1]);
+	}
+	return v[n - 1];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	cout << CountAndSay(n) << '\n';	
+	return 0;	
+}
